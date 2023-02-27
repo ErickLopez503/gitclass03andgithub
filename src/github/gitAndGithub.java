ec8f014b76b1ee11f49dd96748f982df6d896331
erickLop503 <4everklopez@gmail.com>
2023-02-26 23:21:51 -0500

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -2,4 +2,9 @@ package github;
 
 public class gitAndGithub {
 
+	public static void main(String[] args) {
+		
+		System.out.println("today we learn github");
+		System.out.println("Today is a really good day");
+	}
 }
