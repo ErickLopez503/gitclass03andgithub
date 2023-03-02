ca20444c5fac57a88208d1a147001f93ab82b4c5
erickLop503 <4everklopez@gmail.com>
2023-03-01 23:27:24 -0500

    another code created

---
+++ src/newPackageInGit/NewClassInGit.java
@@ -0,0 +1,19 @@
+package newPackageInGit;
+
+public class NewClassInGit {
+
+	public static void main(String[] args) {
+
+		int a = 23;
+		int b = 4;
+		
+		if (a>=b) {
+			
+			System.out.println("Is true");
+		} else {
+			
+			System.out.println("Is not true");
+		}
+
+	}
+}
